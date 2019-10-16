7f04a5770ffa4684346adb4abd9fe081cf1f6304
OlenkoStas <stas310785@gmail.com>
2019-10-16 19:58:50 +0300

    Init

---
+++ Guide/Guide/Form1.Designer.cs
@@ -0,0 +1,252 @@
+﻿namespace Guide
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.treeView = new System.Windows.Forms.TreeView();
+            this.listView = new System.Windows.Forms.ListView();
+            this.cbDrives = new System.Windows.Forms.ComboBox();
+            this.cbListViewStyle = new System.Windows.Forms.ComboBox();
+            this.tbCurrentFullPath = new System.Windows.Forms.TextBox();
+            this.splitContainer = new System.Windows.Forms.SplitContainer();
+            this.btnBack = new System.Windows.Forms.Button();
+            this.pbIcon = new System.Windows.Forms.PictureBox();
+            this.lblType = new System.Windows.Forms.Label();
+            this.lblSize = new System.Windows.Forms.Label();
+            this.lblDate = new System.Windows.Forms.Label();
+            this.tbSelectedDisk = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer)).BeginInit();
+            this.splitContainer.Panel1.SuspendLayout();
+            this.splitContainer.Panel2.SuspendLayout();
+            this.splitContainer.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbIcon)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // treeView
+            // 
+            this.treeView.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.treeView.Location = new System.Drawing.Point(0, 35);
+            this.treeView.Name = "treeView";
+            this.treeView.Size = new System.Drawing.Size(255, 465);
+            this.treeView.TabIndex = 0;
+            this.treeView.BeforeCollapse += new System.Windows.Forms.TreeViewCancelEventHandler(this.treeView_BeforeCollapse);
+            this.treeView.BeforeExpand += new System.Windows.Forms.TreeViewCancelEventHandler(this.treeView_BeforeExpand);
+            this.treeView.NodeMouseClick += new System.Windows.Forms.TreeNodeMouseClickEventHandler(this.treeView_NodeMouseClick);
+            // 
+            // listView
+            // 
+            this.listView.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listView.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.listView.Location = new System.Drawing.Point(0, 35);
+            this.listView.Name = "listView";
+            this.listView.Size = new System.Drawing.Size(507, 465);
+            this.listView.TabIndex = 1;
+            this.listView.UseCompatibleStateImageBehavior = false;
+            this.listView.SelectedIndexChanged += new System.EventHandler(this.listView_SelectedIndexChanged);
+            this.listView.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.listView_MouseDoubleClick);
+            // 
+            // cbDrives
+            // 
+            this.cbDrives.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.cbDrives.FormattingEnabled = true;
+            this.cbDrives.Location = new System.Drawing.Point(129, 5);
+            this.cbDrives.Name = "cbDrives";
+            this.cbDrives.Size = new System.Drawing.Size(122, 21);
+            this.cbDrives.TabIndex = 2;
+            this.cbDrives.DropDown += new System.EventHandler(this.cbDrives_DropDown);
+            this.cbDrives.SelectedIndexChanged += new System.EventHandler(this.cbDrives_SelectedIndexChanged);
+            // 
+            // cbListViewStyle
+            // 
+            this.cbListViewStyle.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.cbListViewStyle.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.cbListViewStyle.FormattingEnabled = true;
+            this.cbListViewStyle.Location = new System.Drawing.Point(383, 5);
+            this.cbListViewStyle.Name = "cbListViewStyle";
+            this.cbListViewStyle.Size = new System.Drawing.Size(119, 21);
+            this.cbListViewStyle.TabIndex = 3;
+            this.cbListViewStyle.SelectedIndexChanged += new System.EventHandler(this.cbListViewStyle_SelectedIndexChanged);
+            // 
+            // tbCurrentFullPath
+            // 
+            this.tbCurrentFullPath.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tbCurrentFullPath.Font = new System.Drawing.Font("Arial Rounded MT Bold", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbCurrentFullPath.ForeColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.tbCurrentFullPath.Location = new System.Drawing.Point(59, 5);
+            this.tbCurrentFullPath.Name = "tbCurrentFullPath";
+            this.tbCurrentFullPath.Size = new System.Drawing.Size(319, 21);
+            this.tbCurrentFullPath.TabIndex = 4;
+            this.tbCurrentFullPath.TextChanged += new System.EventHandler(this.tbFullPath_TextChanged);
+            // 
+            // splitContainer
+            // 
+            this.splitContainer.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.splitContainer.Location = new System.Drawing.Point(9, 6);
+            this.splitContainer.Name = "splitContainer";
+            // 
+            // splitContainer.Panel1
+            // 
+            this.splitContainer.Panel1.Controls.Add(this.tbSelectedDisk);
+            this.splitContainer.Panel1.Controls.Add(this.treeView);
+            this.splitContainer.Panel1.Controls.Add(this.cbDrives);
+            // 
+            // splitContainer.Panel2
+            // 
+            this.splitContainer.Panel2.Controls.Add(this.btnBack);
+            this.splitContainer.Panel2.Controls.Add(this.listView);
+            this.splitContainer.Panel2.Controls.Add(this.cbListViewStyle);
+            this.splitContainer.Panel2.Controls.Add(this.tbCurrentFullPath);
+            this.splitContainer.Size = new System.Drawing.Size(763, 505);
+            this.splitContainer.SplitterDistance = 254;
+            this.splitContainer.TabIndex = 5;
+            // 
+            // btnBack
+            // 
+            this.btnBack.BackColor = System.Drawing.Color.PowderBlue;
+            this.btnBack.Location = new System.Drawing.Point(3, 3);
+            this.btnBack.Name = "btnBack";
+            this.btnBack.Size = new System.Drawing.Size(50, 26);
+            this.btnBack.TabIndex = 5;
+            this.btnBack.Text = "Back";
+            this.btnBack.UseVisualStyleBackColor = false;
+            this.btnBack.Click += new System.EventHandler(this.btnBack_Click);
+            // 
+            // pbIcon
+            // 
+            this.pbIcon.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.pbIcon.BackColor = System.Drawing.Color.LightSkyBlue;
+            this.pbIcon.Location = new System.Drawing.Point(9, 512);
+            this.pbIcon.Name = "pbIcon";
+            this.pbIcon.Size = new System.Drawing.Size(60, 48);
+            this.pbIcon.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pbIcon.TabIndex = 6;
+            this.pbIcon.TabStop = false;
+            // 
+            // lblType
+            // 
+            this.lblType.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblType.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lblType.ForeColor = System.Drawing.Color.Blue;
+            this.lblType.Location = new System.Drawing.Point(75, 514);
+            this.lblType.Name = "lblType";
+            this.lblType.Size = new System.Drawing.Size(218, 48);
+            this.lblType.TabIndex = 7;
+            // 
+            // lblSize
+            // 
+            this.lblSize.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.lblSize.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lblSize.ForeColor = System.Drawing.Color.Blue;
+            this.lblSize.Location = new System.Drawing.Point(534, 514);
+            this.lblSize.Name = "lblSize";
+            this.lblSize.Size = new System.Drawing.Size(240, 46);
+            this.lblSize.TabIndex = 8;
+            // 
+            // lblDate
+            // 
+            this.lblDate.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.lblDate.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lblDate.ForeColor = System.Drawing.Color.Blue;
+            this.lblDate.Location = new System.Drawing.Point(299, 516);
+            this.lblDate.Name = "lblDate";
+            this.lblDate.Size = new System.Drawing.Size(229, 46);
+            this.lblDate.TabIndex = 9;
+            // 
+            // tbSelectedDisk
+            // 
+            this.tbSelectedDisk.BackColor = System.Drawing.Color.LightSkyBlue;
+            this.tbSelectedDisk.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.tbSelectedDisk.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.tbSelectedDisk.Location = new System.Drawing.Point(3, 8);
+            this.tbSelectedDisk.Name = "tbSelectedDisk";
+            this.tbSelectedDisk.Size = new System.Drawing.Size(123, 15);
+            this.tbSelectedDisk.TabIndex = 3;
+            this.tbSelectedDisk.Text = "Selected Drive";
+            this.tbSelectedDisk.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightSkyBlue;
+            this.ClientSize = new System.Drawing.Size(784, 562);
+            this.Controls.Add(this.lblDate);
+            this.Controls.Add(this.lblSize);
+            this.Controls.Add(this.lblType);
+            this.Controls.Add(this.pbIcon);
+            this.Controls.Add(this.splitContainer);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MinimumSize = new System.Drawing.Size(800, 600);
+            this.Name = "Form1";
+            this.Text = "My Guide";
+            this.splitContainer.Panel1.ResumeLayout(false);
+            this.splitContainer.Panel1.PerformLayout();
+            this.splitContainer.Panel2.ResumeLayout(false);
+            this.splitContainer.Panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer)).EndInit();
+            this.splitContainer.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pbIcon)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TreeView treeView;
+        private System.Windows.Forms.ListView listView;
+        /// <summary>
+        /// Список логических дисков
+        /// </summary>
+        private System.Windows.Forms.ComboBox cbDrives;
+        /// <summary>
+        /// Список возможных отображений элементов
+        /// </summary>
+        private System.Windows.Forms.ComboBox cbListViewStyle;
+        /// <summary>
+        /// Строка полного текущего пути выбраного элемента
+        /// </summary>
+        private System.Windows.Forms.TextBox tbCurrentFullPath;
+        private System.Windows.Forms.SplitContainer splitContainer;
+        private System.Windows.Forms.Button btnBack;
+        private System.Windows.Forms.PictureBox pbIcon;
+        private System.Windows.Forms.Label lblType;
+        private System.Windows.Forms.Label lblSize;
+        private System.Windows.Forms.Label lblDate;
+        private System.Windows.Forms.TextBox tbSelectedDisk;
+    }
+}
+
---
+++ Guide/Guide/Form1.cs
@@ -0,0 +1,334 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Diagnostics;
+using System.Windows.Forms;
+
+namespace Guide
+{
+
+    public partial class Form1 : Form
+    {
+        FileSystemWatcher _watcher = new FileSystemWatcher();
+        /// <summary>
+        /// Список посещаемых каталогов(для возврата назад)
+        /// </summary>
+        List<string> _navigatePathList = new List<string>();
+        public Form1()
+        {
+            InitializeComponent();
+            InitcbDrive();
+            InitImageList();
+            InitcbListViewStyle();
+            _watcher.Created += WatcherCreate;
+            _watcher.Deleted += WatcherDeleted;
+            listView.FullRowSelect = true;
+        }
+        /// <summary>
+        /// Выдает сообщение при удалении из текущей папки
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void WatcherDeleted(object sender, FileSystemEventArgs e)
+        {
+            string msg = "Удаление файла или каталога!";
+            MessageBox.Show(msg, "Сообщение", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+        /// <summary>
+        /// Выдает сообщение при создании папки в текущем каталоге
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void WatcherCreate(object sender, FileSystemEventArgs e)
+        {
+            string msg = "Создание каталога!";
+            MessageBox.Show(msg, "Сообщение", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+        /// <summary>
+        /// Подключает FileSystemWatcher к текущему каталогу
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void tbFullPath_TextChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                _navigatePathList.Add(tbCurrentFullPath.Text);
+                _watcher.Path = tbCurrentFullPath.Text;
+                _watcher.EnableRaisingEvents = true;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Внимание", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        /// <summary>
+        /// Инициализирует список стилей отображения элементов в ListView
+        /// </summary>
+        private void InitcbListViewStyle()
+        {
+            cbListViewStyle.Items.AddRange(Enum.GetNames(typeof(View)));
+            cbListViewStyle.SelectedIndex = 0;
+        }
+
+        /// <summary>
+        /// Инициализирует список доступных дисков на пк
+        /// </summary>
+        private void InitcbDrive()
+        {
+            cbDrives.Items.AddRange(Environment.GetLogicalDrives());
+            cbDrives.SelectedIndex = 0;
+            //сразу отображает элементы текущего логического диска
+            ShowListViewContent(cbDrives.SelectedItem as string);
+        }
+        /// <summary>
+        /// Проверка на наличие новых устройств в системе(флешка)
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void cbDrives_DropDown(object sender, EventArgs e)
+        {
+            if (cbDrives.Items.Count != Environment.GetLogicalDrives().Count())
+            {
+                cbDrives.Items.Clear();
+                cbDrives.Items.AddRange(Environment.GetLogicalDrives());
+            }
+        }
+        /// <summary>
+        /// Изменяет текущий логический диск
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void cbDrives_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (cbDrives.SelectedIndex != -1)
+            {
+                InitTree(cbDrives.SelectedItem as string);
+            }
+        }
+
+        /// <summary>
+        /// Инициализирует список картинок для TreeView и ListView
+        /// </summary>
+        private void InitImageList()
+        {
+            ImageList imageList = new ImageList();
+            imageList.Images.Add(Image.FromFile("Images/FolderClosed.png"));
+            imageList.Images.Add(Image.FromFile("Images/FolderOpen.png"));
+            imageList.Images.Add(Image.FromFile("Images/File.png"));
+            imageList.ImageSize = new Size(24, 24);
+
+            ImageList largeList = new ImageList();
+            largeList.Images.Add(Image.FromFile("Images/FolderClosed.png"));
+            largeList.Images.Add(Image.FromFile("Images/FolderOpen.png"));
+            largeList.Images.Add(Image.FromFile("Images/File.png"));
+            largeList.ImageSize = new Size(36, 36);
+            //для ListView
+            listView.SmallImageList = imageList;
+            listView.LargeImageList = largeList;
+            //для TreeView
+            treeView.ImageList = imageList;
+        }
+        /// <summary>
+        /// Начальная инициализация дерева
+        /// </summary>
+        /// <param name="path"></param>
+        private void InitTree(string path)
+        {
+            treeView.Nodes.Clear();
+            if (Directory.Exists(path))
+            {
+                foreach (var dir in Directory.GetDirectories(path))
+                {
+                    DirectoryInfo di = new DirectoryInfo(dir);
+                    TreeNode node = new TreeNode(di.Name, 0, 1);
+                    node.Nodes.Add("");
+                    node.Tag = di.FullName;
+                    treeView.Nodes.Add(node);
+                }
+            }
+        }
+        /// <summary>
+        /// Заполняет выбраный узел элементами
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void treeView_BeforeExpand(object sender, TreeViewCancelEventArgs e)
+        {
+            treeView.SelectedNode = e.Node;
+            //Отображает на экране текущий путь
+            tbCurrentFullPath.Text = e.Node.Tag.ToString();
+            string path = e.Node.Tag.ToString();
+
+            e.Node.Nodes.Clear();
+            try
+            {
+                foreach (var dir in Directory.GetDirectories(path))
+                {
+                    DirectoryInfo di = new DirectoryInfo(dir);
+                    TreeNode node = new TreeNode(di.Name, 0, 0);
+                    node.Nodes.Add("");
+                    node.Tag = di.FullName;
+                    e.Node.Nodes.Add(node);
+                }
+            }
+            catch (Exception)
+            {
+                //Отказано в доступе к каталогу 
+            }
+        }
+        /// <summary>
+        /// Изменение стиля отображения в ListView
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void cbListViewStyle_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (cbListViewStyle.SelectedIndex != -1)
+            {
+                string style = cbListViewStyle.SelectedItem as string;
+                listView.View = (View)Enum.Parse(typeof(View), style);
+            }
+        }
+        /// <summary>
+        /// По клику по узлу - отображает элементы в ListView
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void treeView_NodeMouseClick(object sender, TreeNodeMouseClickEventArgs e)
+        {
+            ShowListViewContent(e.Node.Tag.ToString());
+            string path = e.Node.Tag.ToString();
+            if (Directory.Exists(path))
+            {
+                try
+                {
+                    pbIcon.ImageLocation = @"Images\FolderClosed.png";
+                    lblType.Text = $"Элементов : {Directory.GetDirectories(path).Count() + Directory.GetFiles(path).Count()}";
+                }
+                catch(Exception)
+                {
+                    //Нет прав доступа к каталогу
+                }
+            }
+        }
+        /// <summary>
+        /// Отображает все элементы текущего каталога
+        /// </summary>
+        /// <param name="path">Путь к каталогу или файлу</param>
+        private void ShowListViewContent(string path)
+        {
+            try
+            {
+                if (Directory.Exists(path))
+                {
+                    tbCurrentFullPath.Text = path;
+                    listView.Clear();
+                    listView.Columns.Add(new ColumnHeader { Text = "Name", Width = 150 });
+                    listView.Columns.Add(new ColumnHeader { Text = "Type" });
+                    listView.Columns.Add(new ColumnHeader { Text = "Size" });
+                    foreach (var dir in Directory.GetDirectories(path))
+                    {
+                        DirectoryInfo di = new DirectoryInfo(dir);
+                        ListViewItem lv = new ListViewItem(di.Name, 0);
+                        lv.Tag = di.FullName;
+                        lv.SubItems.Add(new ListViewItem.ListViewSubItem(lv, "Папка"));
+                        listView.Items.Add(lv);
+                    }
+                    foreach (var dir in Directory.GetFiles(path))
+                    {
+                        FileInfo fi = new FileInfo(dir);
+                        ListViewItem lv = new ListViewItem(fi.Name, 2);
+                        lv.Tag = fi.FullName;
+                        lv.SubItems.Add(new ListViewItem.ListViewSubItem(lv, "Файл"));
+                        lv.SubItems.Add(new ListViewItem.ListViewSubItem(lv, (fi.Length / 1000).ToString() + " КБ"));
+                        listView.Items.Add(lv);
+                    }
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Внимание", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                //каталог недоступен
+            }
+        }
+        /// <summary>
+        /// Переход к дочернему элементу или запуск файла
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void listView_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            ListView senderList = (ListView)sender;
+            ListViewItem clickedItem = senderList.HitTest(e.Location).Item;
+            if (clickedItem != null && File.Exists(clickedItem.Tag.ToString()))
+            {
+                Process.Start(clickedItem.Tag.ToString());
+            }
+            else if (clickedItem != null)
+            {
+                tbCurrentFullPath.Text = clickedItem.Tag.ToString();
+                ShowListViewContent(clickedItem.Tag.ToString());
+            }
+        }
+        /// <summary>
+        /// Изменяет отображаемый текущий путь на перед закрытием узла
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void treeView_BeforeCollapse(object sender, TreeViewCancelEventArgs e)
+        {
+            tbCurrentFullPath.Text = e.Node.Tag.ToString();
+        }
+        /// <summary>
+        /// Возвращает на предыдущий уровень
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnBack_Click(object sender, EventArgs e)
+        {
+            if (_navigatePathList.Count != 0 && _navigatePathList.Count != 1)
+            {
+                _navigatePathList.RemoveAt(_navigatePathList.Count - 1);
+                tbCurrentFullPath.Text = _navigatePathList.Last();
+                ShowListViewContent(tbCurrentFullPath.Text);
+                _navigatePathList.RemoveAt(_navigatePathList.Count - 1);
+            }
+        }
+        /// <summary>
+        /// Показывает дополнительную информацию об элементе в низу окна
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void listView_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            var item = listView.SelectedItems;
+            if (item.Count == 1)
+            {
+                string path = item[0].Tag.ToString();
+                if (Directory.Exists(path))
+                {
+                    DirectoryInfo info = new DirectoryInfo(path);
+                    pbIcon.ImageLocation = @"Images\FolderOpen.png";
+                    lblType.Text = $"{info.Name}\nПапка с файлами";
+                    lblDate.Text = $"Дата создания\n{info.CreationTime}";
+                    lblSize.Text = "";
+                }
+                else if (File.Exists(path))
+                {
+                    FileInfo info = new FileInfo(path);
+                    pbIcon.ImageLocation = @"Images\File.png";
+                    lblType.Text = $"{info.Name}\nФайл";
+                    lblDate.Text = $"Дата создания\n{info.CreationTime}";
+                    lblSize.Text = $"Размер \n{info.Length / 1000} КБ";
+                }
+            }
+        }
+    }
+}
